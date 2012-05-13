b8452803ac008a9be1fd572f9d42549ff50a0c90
Pavel fjfalcon Chernyak <fjfalcon@fjfalcon.ru>
2012-05-13 15:25:49 +0400

    Continue to make devtree

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := pyramid
+PRODUCT_RELEASE_NAME := flyiq285
 
 TARGET_BOOTANIMATION_NAME := vertical-540x960
 
@@ -10,14 +10,14 @@ TARGET_BOOTANIMATION_NAME := vertical-540x960
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/pyramid/device_pyramid.mk)
+$(call inherit-product, device/fly/iq285/device.mk)
 
 # Device naming
-PRODUCT_DEVICE := pyramid
-PRODUCT_NAME := cm_pyramid
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := HTC Sensation
-PRODUCT_MANUFACTURER := HTC
+PRODUCT_DEVICE := fluiq85
+PRODUCT_NAME := cm_iq285
+PRODUCT_BRAND := Fly
+PRODUCT_MODEL := Fly Turbo IQ285
+PRODUCT_MANUFACTURER := Fly
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_pyramid BUILD_FINGERPRINT=htc_europe/htc_pyramid/pyramid:4.0.3/IML74K/309896.3:user/release-keys PRIVATE_BUILD_DESC="3.32.401.3 CL309896 release-keys" BUILD_NUMBER=309896
--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASE=../../../vendor/htc/pyramid/proprietary
+BASE=../../../vendor/fly/iq285/proprietary
 rm -rf $BASE/*
 
 for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
